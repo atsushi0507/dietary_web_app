e29c4566cf63033fd69fd7af90ead70c995fbcc6
Atsushi Mizukami <a.mizukami.0507@gmail.com>
2024-10-23 23:29:46 +0900

    Update weightRecord to save the weight record to the local storage

--- backend/config/config.py
+++ backend/config/config.py
@@ -1,13 +1,15 @@
 import json
+from pathlib import Path
 from google.cloud import bigquery
 from google.oauth2 import service_account
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 # サービスアカウントキーのパス (適切なキーを指定)
-SERVICE_ACCOUNT_FILE = '/path/to/your/service-account.json'
+SERVICE_ACCOUNT_FILE = BASE_DIR / 'config/dietary-web-app-5e82d25a492c.json'
 
 # BigQuery クライアントの作成
 credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE)
-client = bigquery.Client(credentials=credentials, project='your-gcp-project')
+client = bigquery.Client(credentials=credentials, project='dietary-web-app')
 
 # テーブル情報 (適宜変更)
-DATASET_NAME = 'your_dataset'
+DATASET_NAME = 'app_data'
