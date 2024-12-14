7723791cd5e031f73fce573cdfb8da2dc1eae145
Atsushi Mizukami <a.mizukami.0507@gmail.com>
2024-12-14 20:16:09 +0900

    Update LP

--- frontend/src/components/templates/landingpage/components/Card.jsx
+++ frontend/src/components/templates/landingpage/components/Card.jsx
@@ -1,18 +1,61 @@
-import styled from 'styled-components';
+import Button from "@/components/atoms/Button";
+import { Stack, Typography } from "@mui/material";
+import { useRouter } from "next/navigation";
+import React from "react";
+import styled from "styled-components";
 
-const CardArea = styled.div`
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  padding: 16px;
-  border: 2px solid #ccc;
-  width: 95%;
-  height: 150px;
-  display: flex;
-  flex-direction: row;
-  background-color: #f5f5f5;
-  border-radius: 4px;
-  background-image: ${(props) => `url(${props.backgroundImage})`};
-  background-size: cover;
-  background-position: center;
-`;
+const Card = () => {
+    const router = useRouter();
 
-export default CardArea;
+    const handleButton = () => {
+        router.push("register_login");
+    }
+    return (
+        <Container>
+            <Stack
+                spacing={2}
+                useFlexGap
+                sx={{ alignItems: "center", width: {xs: "100%", sm: "70%"}}}
+            >
+                <Typography
+                    variant="h2"
+                    align="center"
+                    sx={{
+                        display: "flex",
+                        flexDirection: {sx: "column", sm: "row"},
+                        alignItems: "center"
+                    }}
+                >
+                    食生活を変える<br/>第一歩を踏み出そう
+                </Typography>
+
+                <Typography
+                    sx={{
+                        textAlign: "center",
+                        color: "text.secondary",
+                        width: {sm: "100%", md: "80%"}
+                    }}
+                >
+                “食事スコアは、私の食生活を改善するために非常に役立ちました。視覚的なフィードバックと具体的なアドバイスがとても助かりました。” - Addison Adams
+                </Typography>
+                <Button onClick={handleButton}>
+                    始めてみる
+                </Button>
+            </Stack>
+        </Container>
+    );
+};
+
+export default Card;
+
+const Container = styled.div`
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    padding: 16px;
+    border: 2px solid #ccc;
+    width: 95%;
+    height: auto;
+    display: flex;
+    background-color: #f5f5f5;
+    border-radius: 4px;
+    margin: 8px;
+`
